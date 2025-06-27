3cb5c5492524786527096fef2121f966fbe89fcc
rishikajaiswal <jaisrishika@gmail.com>
2025-06-27 22:21:48 +0530

    deployed server added

---
+++ src/utils/ApiError.ts
@@ -0,0 +1,27 @@
+class ApiError extends Error{
+  statusCode: number;
+  data: any;
+  success: boolean;
+  errors: any[];
+
+  constructor(
+    statusCode: number,
+    message = "Something Went Wrong",
+    errors: any[] = [],
+    stack = ""
+  ) {
+        super(message)
+        this.statusCode=statusCode
+        this.data=null
+        this.message=message
+        this.success= false
+        this.errors=errors
+
+        if(stack){
+            this.stack= stack
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+export default ApiError
